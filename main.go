dbe2332cd864445e0d88ed40d6f5cee40d92fa9f
Yousuf <yousuf@yusuf.local>
2023-10-21 22:52:49 +0600

    add some basic config

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	r := gin.Default()
-
+	r.Use(gin.Logger())
+	r.ForwardedByClientIP = true
+	r.SetTrustedProxies([]string{"127.0.0.1", "192.168.1.2", "10.0.0.0/8"})
 	models.ConnectDatabase()
 
 	r.GET("/books", controllers.FindBooks)
