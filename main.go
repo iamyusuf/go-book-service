d97cce877b7ca5b2082aad2b28ee3bca6d68f917
Yousuf <yousuf@yusuf.local>
2023-10-21 23:01:15 +0600

    write a helper function to define all the routes

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"ozone.com/book-service/controllers"
 	"ozone.com/book-service/models"
 )
 
@@ -12,12 +11,6 @@ func main() {
 	r.ForwardedByClientIP = true
 	r.SetTrustedProxies([]string{"127.0.0.1", "192.168.1.2", "10.0.0.0/8"})
 	models.ConnectDatabase()
-
-	r.GET("/books", controllers.FindBooks)
-	r.POST("/books", controllers.CreateBook)
-	r.GET("/authors", controllers.FindAuthors)
-	r.POST("/authors", controllers.CreateAuthor)
-	r.GET("/authors/:id", controllers.FindAuthor)
-
+	DefineRoutes(r)
 	r.Run()
 }
