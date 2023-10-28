a8feebc0d9c94beb3b7a9fb5028dd40d639a3d47
Md Yousuf Ali <yousuf@yusuf.local>
2023-10-28 15:17:12 +0600

    write a api to ping to the server

--- helpers.go
+++ helpers.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"ozone.com/book-service/controllers"
 )
 
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
 func DefineRoutes(r *gin.Engine) {
 	// Define and set up routes here
+	r.GET("/ping", pong)
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
 	r.GET("/authors", controllers.FindAuthors)
